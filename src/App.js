6222ef4ff29be1f6e3a16ea71e88f2d50636dc45
Shreyas Kale <kaleshreyas45@gmail.com>
2023-02-18 20:35:37 +0530

    seventh commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 function App() {
-  const [data,setData] = useState(false)
   const [popup,setpopup] = useState(false)
   const openWindow = ()=>{
     window.open('/popup?code=1234','test','width=600,height=600,top=100,left=100')
-  }
-  window.callParent = function(){
-    setData(true)
   }
   useEffect(()=>{
     const code = window.location.href.split('=')[1]
     if(code){
         setpopup(true)
         setTimeout(()=>{
-            window.opener.callParent()
+            window.opener.location.reload()
             window.close()            
         },3000)
     }
@@ -23,7 +19,7 @@ function App() {
   return (
     <>
     {popup && <h1>popup</h1>}
-      {data?<h1>Shreyas</h1>:""}
+      <h1>Main window</h1>
       <button onClick={openWindow}>Click</button>
     </>
 
