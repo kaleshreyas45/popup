90c8492879877806f74a64c47992fdbc1f96c84a
Shreyas Kale <kaleshreyas45@gmail.com>
2023-02-18 20:28:21 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import { Route } from 'react-router-dom';
-import Param from './Param';
 import { useState } from 'react';
+import { useEffect } from 'react';
 function App() {
   const [data,setData] = useState(false)
+  const [popup,setpopup] = useState(false)
   const openWindow = ()=>{
-    window.open('/popup/Param?code=1234','test','width=600,height=600,top=100,left=100')
+    window.open('/popup?code=1234','test','width=600,height=600,top=100,left=100')
   }
   window.callParent = function(){
     setData(true)
   }
+  useEffect(()=>{
+    const code = window.location.href.split('=')[1]
+    if(code){
+        setpopup(true)
+        setTimeout(()=>{
+            window.opener.callParent()
+            window.close()            
+        },3000)
+    }
+  },[])
   return (
     <>
+    {popup && <h1>popup</h1>}
       {data?<h1>Shreyas</h1>:""}
       <button onClick={openWindow}>Click</button>
-      <Route path="/popup/Param">
-        <Param></Param>
-      </Route>
     </>
 
   );
